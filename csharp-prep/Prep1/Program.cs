26c96bfcbb0f7625a7e83efba9508f63b36899f3
isaacvgn@gmail.com <itu22002@byui.edu>
2023-06-11 19:29:57 -0600

    W1.r

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}");
     }
+}
+
+
+namespace SpaceForNumbers
+{
+class Program1
+{
+    static void Main(int[] args)
+    {
+        Console.WriteLine("Give me a number! ");
+        int FirstNum = Console.ReadLine();
+
+        Console.WriteLine("Give me another numver! ");
+        int SecondNum = Console.ReadLine();
+
+        Console.WriteLine($"The numbers are{FirstNum} and {SecondNum}");
+    }
+}
+
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,41 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int number =  PromptUserNumber();
+        int sqnum = SquareNumber(number);
+        DisplayResult(name, sqnum);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine("Hello, {0}! Your favorite number squared is: {1}", name, squaredNumber);
+    }
+
 }
